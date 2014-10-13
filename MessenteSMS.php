f4dea27d589ef964f3d977e9026373935549b48f
Rainari <info@netpro.ee>
2014-10-13 22:16:04 +0300

    first commit

---
+++ MessenteSMS.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace netpro9\y2messente;
+
+use SoapClient;
+use yii\base\Component;
+use yii\base\ErrorException;
+use yii\base\InvalidConfigException;
+
+/**
+ * TurboSMS component gives you an ability to send SMS messages via TurboSMS.ua SMS gateway.
+ *
+ * This component is built with use of TurboSMS SOAP service.
+ *
+ * @link      http://turbosms.ua/soap.html TurboSMS SOAP service description.
+ * @link      http://kop.github.io/yii2-turbosms Y2SP project page.
+ * @license   https://github.com/kop/yii2-turbosms/blob/master/LICENSE.md MIT
+ *
+ * @author    Ivan Koptiev <ivan.koptiev@codex.systems>
+ * @version   1.0
+ */
+class MessenteSMS extends Component
+{
+}
